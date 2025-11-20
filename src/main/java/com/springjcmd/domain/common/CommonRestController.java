192d8b1bb090b2aea92a80dbbbf6a70f41cce446
cookooyas <cookooyas@gmail.com>
2025-11-20 09:15:06 +0900

    fix : RestController Request Mapping 변경

--- src/main/java/com/springjcmd/domain/common/CommonRestController.java
+++ src/main/java/com/springjcmd/domain/common/CommonRestController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/common")
-public class CommonController {
-	@GetMapping(value = "/healthCheck.do")
+@RequestMapping("/api")
+public class CommonRestController {
+	@GetMapping(value = "/healthCheck")
 	public String healthCheck() {
 		return "OK";
 	}
